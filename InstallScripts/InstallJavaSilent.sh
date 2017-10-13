e296e218f6ef3a6ac8d62c8bd8a7eb1627d8cfe0
melvintl <melvinl@gmail.com>
2017-10-13 09:50:01 +0100

    updated scripts

---
+++ InstallScripts/InstallJavaSilent.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get install --assume-yes python-software-properties debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install --assume-yes oracle-java8-installer
